e5e6566a62a722a89b97ba647a09aa8760b27d72
jaime-rey <jaimereycasado@gmail.com>
2023-12-10 04:30:07 +0100

    Initial commit

---
+++ src/main/java/dev/jaimerey/rabbitmq/config/RabbitMQConfig.java
@@ -0,0 +1,40 @@
+package dev.jaimerey.rabbitmq.config;
+
+import org.springframework.amqp.core.Binding;
+import org.springframework.amqp.core.BindingBuilder;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.core.TopicExchange;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class RabbitMQConfig {
+
+    @Value("${rabbitmq.queue.name}")
+    private String queue;
+
+    @Value("${rabbitmq.exchange.name}")
+    private String exchange;
+
+    @Value("${rabbitmq.routing.key}")
+    private String routingKey;
+
+    // spring bean for rabbitmq queue
+    @Bean
+    public Queue queue(){
+        return new Queue(queue);
+    }
+    // spring bean for rabbitmq topic exchange
+    @Bean
+    public TopicExchange exchange(){
+        return new TopicExchange(exchange);
+    }
+    // binding the queue to the exchange with the routing key
+    @Bean
+    public Binding binding(){
+        return BindingBuilder.bind(queue()).to(exchange()).with(routingKey);
+    }
+
+    // ConnectionFactory
+}
